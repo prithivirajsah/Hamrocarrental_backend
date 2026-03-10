7b4523fe57088a57ac7fdd29a098585f918b9114
Prithivi Raj Sah <prithivirajsah@Prithivis-MacBook-Air.local>
2026-03-10 19:08:06 +0545

    Rating Sysytem

--- routers/contact.py
+++ routers/contact.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
-from flask_jwt_extended import jwt_required
+# from flask_jwt_extended import jwt_required
 from database_connection import get_db
 from crud.contact import create_contact_message
 from schemas.contact import ContactMessageCreate, ContactSubmitResponse
@@ -11,7 +11,7 @@ router = APIRouter(prefix="/contact", tags=["Contact"])
 
 @router.post("", response_model=ContactSubmitResponse, status_code=status.HTTP_201_CREATED)
 #$check authentication and token
-@jwt_required()
+# @jwt_required()
 def submit_contact_form(payload: ContactMessageCreate, db: Session = Depends(get_db)):
     contact = create_contact_message(db, payload)
     return {
